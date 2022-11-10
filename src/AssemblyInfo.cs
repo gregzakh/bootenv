c0cbea2400860210ad8f6d45caeb7b6e7c9f272b
gregzakh <grishanz@yandex.com>
2022-11-11 01:15:57 +0500

    M src/AssemblyInfo.cs

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 using System;
 using System.Reflection;
 
+[assembly: AssemblyDescription("Check boot mode: UEFI or Legacy")]
+[assembly: AssemblyTitle("bootenv")]
 [assembly: AssemblyVersion("1.0.0.0")]
